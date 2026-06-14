1b953de27a5f026c66fbbf9631030e9c568afeca
kenshin0413 <kenshin1234569@gmail.com>
2026-06-14 14:34:10 +0900

    初期

---
+++ RoachBreeder/RoachBreeder/RoachBreederApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoachBreederApp.swift
+//  RoachBreeder
+//
+//  Created by miyamotokenshin on R 8/06/14.
+//
+
+import SwiftUI
+
+@main
+struct RoachBreederApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
